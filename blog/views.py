6a44a14d7ca7539f2d4aa4c9e914225d799edc6a
tae20n <djs99870@naver.com>
2023-08-04 16:11:37 +0900

    포스트 목록에 부트스트랩 적용

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 class PostList(ListView):
     model = Post
     ordering = '-pk'
+
+class PostDetail(DetailView):
+    model = Post
 
 # Create your views here.
 # def index(request):
@@ -18,13 +21,13 @@ class PostList(ListView):
 #         }
 #     )
 
-def single_post_page(request, pk):
-    post = Post.objects.get(pk=pk)
-
-    return render(
-        request,
-        'blog/single_post_page.html',
-        {
-            'post': post,
-        }
-    )
+# def single_post_page(request, pk):
+#     post = Post.objects.get(pk=pk)
+#
+#     return render(
+#         request,
+#         'blog/post_detail.html',
+#         {
+#             'post': post,
+#         }
+#     )
