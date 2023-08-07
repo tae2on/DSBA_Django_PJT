8f280cdebeb7e5604c0c91d66dde3a7ea0300caa
tae20n <djs99870@naver.com>
2023-08-07 11:39:22 +0900

    다운로드 버튼 출력하기

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,6 @@
+import os.path
+import os
+
 from django.db import models
 
 class Post(models.Model):
@@ -16,4 +19,10 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
+
+    def get_file_name(self):
+        return os.path.basename(self.file_upload.name)
+
+    def get_file_ext(self):
+        return self.get_file_name().split('.')[-1]
 # Create your models here.
