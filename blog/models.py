7478533a2f32db46375eae822d41e152cb1b049e
tae20n <djs99870@naver.com>
2023-08-08 09:19:09 +0900

    포스트에 작성자 필드 추가

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ import os.path
 import os
 
 from django.db import models
+from django.contrib.auth.models import User
 
 class Post(models.Model):
     title = models.CharField(max_length=30)
@@ -13,10 +14,11 @@ class Post(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
-    # author: 추후 작성 예정
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f'[{self.pk}]{self.title}'
+        return f'[{self.pk}]{self.title}::{self.author}'
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
